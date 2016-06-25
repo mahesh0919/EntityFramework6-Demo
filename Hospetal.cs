e7a6b46e1320665e9586a91463fb1e9f34bb3a74
mahesh <mahesh0919@gmail.com>
2016-06-25 12:58:15 +0530

    Code First Approach
    In this commit added initial code to create database from objects

---
+++ Hospetal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityFramework
+{
+    public class Doctor
+    {
+        public Doctor() { }
+
+        public int DoctorID { get; set; }
+        public string DoctorFirstName { get; set; }
+        public string DoctorLastName{ get; set; }
+        public string EmailAddress{ get; set; }
+
+        public virtual ICollection<Appointment> Appointments { get; set; }
+    }
+
+    public class Patient
+    {
+        public Patient() { }
+
+        public int PatientID { get; set; }
+        public string PatientFirstName { get; set; }
+        public string PatientLastName { get; set; }
+        public string Address { get; set; }
+        public virtual ICollection<Appointment> Appointments { get; set; }
+    }
+
+    public class Appointment
+    {
+        public Appointment() { }
+
+        public int AppointmentID { get; set; }
+
+        //Foreign key for Doctor
+        public int DoctorID { get; set; }
+
+        //Foreign key for Patient
+        public int PatientID { get; set; }
+        public DateTime? AppointmentTime { get; set; }
+
+        public virtual Patient Patient { get; set; }
+        public virtual Doctor Doctor { get; set; }
+    }
+
+    public class HospetalContext: DbContext
+    {
+        public HospetalContext()
+            : base("HospetalDB")   // if connection string HospetalDB not found in app.config,it will throw exception
+        { 
+        
+        }
+
+        public virtual DbSet<Doctor> Doctors { get; set; }
+        public virtual DbSet<Patient> Patients { get; set; }
+        public virtual DbSet<Appointment> Appointments { get; set; }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddRecords();
+            
+        }
+
+        public static void AddRecords()
+        {
+            using (HospetalContext hContext = new HospetalContext())
+            {
+                //Adding new Doctor  ---------------------------------------------
+                Doctor doctor1 = new Doctor()
+                {
+                    DoctorFirstName = "Mahesh",
+                    DoctorLastName = "Pendker",
+                    EmailAddress = "mahesh0919@gmail.com"
+                };
+
+                Doctor doctor2 = new Doctor()
+                {
+                    DoctorFirstName = "Rakesh",
+                    DoctorLastName = "Pendker",
+                    EmailAddress = "rakesh.p@gmail.com"
+                };
+
+                hContext.Doctors.Add(doctor1);
+                hContext.Doctors.Add(doctor2);
+
+                //Adding Patents ---------------------------------------------
+                Patient patient1 = new Patient()
+                { 
+                    Address ="Hyderabad",
+                    PatientFirstName = "Vikram",
+                    PatientLastName = "Voma"
+                };
+
+                Patient patient2 = new Patient()
+                {
+                    Address = "Hyderabad",
+                    PatientFirstName = "Vikram",
+                    PatientLastName = "Voma"
+                };
+
+                hContext.Patients.Add(patient1);
+                hContext.Patients.Add(patient2);
+
+                Appointment appointment1 = new Appointment() { AppointmentTime= DateTime.Now.Date, Doctor = doctor1, Patient = patient1  };
+
+                hContext.Appointments.Add(appointment1);
+
+                hContext.SaveChanges();
+
+                //Display Doctors List
+                var doctors = (from d in hContext.Doctors
+                               orderby d.DoctorFirstName
+                               select d).ToList();
+
+                Console.WriteLine("-------List of doctors------");
+                foreach (Doctor d in doctors)
+                {
+                    Console.WriteLine("Doctor FirstName: " + d.DoctorFirstName);
+                }
+
+                Console.WriteLine("Press any Key");
+                Console.Read();
+            }
+        }
+    
+    }
+}
